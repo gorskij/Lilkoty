19efcf03c57b598b21facc7fc3a1340c7a4d8437
Dzioral <jakub1q23@gmail.com>
2021-06-25 12:25:27 +0200

    cleanup in controller api cats

--- app/controllers/api/v1/cats_controller.rb
+++ app/controllers/api/v1/cats_controller.rb
@@ -8,36 +8,9 @@ module Api
         render json: cats, each_serializer: CatSerializer
       end
 
-      def create
-        cat = Cat.create!(cat_params)
-        if cat
-          render json: cat
-        else
-          render json: cat.errors
-        end
-      end
-
       def show
-        if cat
-          render json: cat
-        else
-          render json: cat.errors
-        end
-      end
-
-      def destroy
-        cat&.destroy
-        render json: { message: 'Cat deleted!' }
-      end
-
-      private
-
-      def cat_params
-        params.permit(:name, :sex, :breed, :status, :breeding, :date_of_birth, images: [])
-      end
-
-      def cat
-        @cat ||= Cat.find(params[:id])
+        cat = Cat.with_attached_images.find(params[:id])
+        render json: cat, serializer: CatSerializer
       end
     end
   end
