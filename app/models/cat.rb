18940d9375c17c55f4f5b031ebf8cee3308d9746
Dzioral <jakub1q23@gmail.com>
2021-07-01 18:26:00 +0200

    validations for cat and his attachments

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -5,8 +5,8 @@ class Cat < ApplicationRecord
   belongs_to :mother, class_name: 'Cat', optional: true
   belongs_to :father, class_name: 'Cat', optional: true
   has_many :images, class_name: 'CatAttachedImage', foreign_key: 'cat_id', dependent: :destroy
-  validates :name, :breed, :status, :breeding, presence: true
-  validate :validate_sex
+  validates :name, :breed, :breeding, presence: true
+  validate :validate_sex, :status
 
   scope :available, -> { where(status: 'available') }
 
@@ -14,5 +14,12 @@ class Cat < ApplicationRecord
 
   def validate_sex
     errors.add :base, 'error sex invalid, can be male or female' if sex != 'male' && sex != 'female'
+  end
+
+  def validate_status
+    if status != 'available' && status != 'not_available'
+      errors.add :base,
+                 'error status invalid, can be available or not_available'
+    end
   end
 end
--- app/models/cat_attached_image.rb
+++ app/models/cat_attached_image.rb
@@ -4,12 +4,17 @@ class CatAttachedImage < ApplicationRecord
   has_one_attached :image
   belongs_to :cat
 
-  validates :role, :date, :cat_id, presence: true
+  validates :date, :cat_id, presence: true
   validate :validate_if_attached
+  validate :validate_role
 
   private
 
   def validate_if_attached
-    errors.add :base, 'cannot create without an image' if image.attached? == false
+    errors.add :base, 'cannot create attachment without an image' if image.attached? == false
+  end
+
+  def validate_role
+    errors.add :base, 'error role invalid, can be profile or default' if role != 'profile' && role != 'default'
   end
 end
