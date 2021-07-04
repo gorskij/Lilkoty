5b937100100b1089dda2c8be42a9e7f753e1a6b1
Oskar Natorski <oski.natorski@gmail.com>
2021-07-04 19:01:12 +0200

    Basic header with links to home and our cats

--- app/javascript/components/Header.jsx
+++ app/javascript/components/Header.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
-import '../../assets/stylesheets/header.scss'
-import '../../assets/stylesheets/application.scss'
-import { Link } from 'react-router-dom'
+import React from "react";
+import "../../assets/stylesheets/header.scss";
+import "../../assets/stylesheets/application.scss";
+import { Link } from "react-router-dom";
 
 class Header extends React.Component {
-  render () {
+  render() {
     return (
-      <div className="Header">
-        <h1>SFPOPOS</h1>
-        <Link to='/litters'>bagno</Link>
+      <div className="header">
+        <Link to="/" className="button">
+          Home
+        </Link>
+        <div className="button">Nasze Koty</div>
+        <Link to="/Litters" className="button">
+          Kocięta
+        </Link>
+        <div className="button">Kontakt</div>
+        <div className="button">Aktualnosci</div>
+        <div className="button">Informacje</div>
+        <div className="button">
+          <div className="social-button">Facebook</div>
+          <div className="social-button">Youtube</div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default Header
+export default Header;
