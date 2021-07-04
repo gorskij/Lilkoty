a50601e53d2252950e719dd9ed1e600aa4b8721a
Dzioral <jakub1q23@gmail.com>
2021-07-04 13:42:34 +0200

    swagger innit

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'draper'
 gem 'haml'
 gem 'pundit'
 gem 'rubocop'
+gem 'rswag'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
--- spec/requests/api/v1/litters_spec.rb
+++ spec/requests/api/v1/litters_spec.rb
@@ -1,10 +1,10 @@
 require 'swagger_helper'
 
-RSpec.describe 'api/v1/cats', type: :request do
+RSpec.describe 'api/v1/litters', type: :request do
 
-  path '/api/v1/cats/index' do
+  path '/api/v1/litters/index' do
 
-    get('list cats') do
+    get('list litters') do
       response(200, 'successful') do
 
         after do |example|
@@ -19,26 +19,9 @@ RSpec.describe 'api/v1/cats', type: :request do
     end
   end
 
-  path '/api/v1/cats/show' do
+  path '/api/v1/litters/show' do
 
-    get('show cat') do
-      response(200, 'successful') do
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
-        run_test!
-      end
-    end
-  end
-
-  path '/api/v1/cats/available' do
-
-    get('available cat') do
+    get('show litter') do
       response(200, 'successful') do
 
         after do |example|
