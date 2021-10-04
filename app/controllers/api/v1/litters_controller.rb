6e7ddfb65278e5c7a50df48051ca602d11f50c10
Jakub Górski <83185535+Dzioral@users.noreply.github.com>
2021-10-05 00:05:46 +0200

    Added endpoint for cats from our breeding (#7)

--- app/controllers/api/v1/litters_controller.rb
+++ app/controllers/api/v1/litters_controller.rb
@@ -4,6 +4,7 @@ module Api
   module V1
     class LittersController < ApplicationController
       def initialize
+        super
         @cat_includes = [
           :profile_image_blob,
           :breed,
